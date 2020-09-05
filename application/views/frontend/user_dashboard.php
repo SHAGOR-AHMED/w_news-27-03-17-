d9a6e275b49f97c131f3e5ec4f763e1cb1ade0fd
shagor <shagor.ahmed374@gmail.com>
2020-09-05 16:02:09 +0600

    This is a mlm software

---
+++ application/views/frontend/user_dashboard.php
@@ -0,0 +1,118 @@
+<div class="container"><br>
+
+    <div class="row profile">
+
+        <div class="col-md-3">
+            <?php include('page/left_sidebar.php') ?>
+        </div>
+        <div class="col-md-9">
+            <h4>Dashboad</h4>
+            <hr/>
+            <div class="panel with-nav-tabs panel-primary">
+                <div class="panel-heading">
+                    ......
+                </div>
+                <div class="panel-body">
+                    <br/>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    Test 1
+                                </div>
+                                <div class="panel-body"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    Test 1
+                                </div>
+                                <div class="panel-body"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    Test 1
+                                </div>
+                                <div class="panel-body"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    Test 1
+                                </div>
+                                <div class="panel-body"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    Test 1
+                                </div>
+                                <div class="panel-body"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    Test 1
+                                </div>
+                                <div class="panel-body"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    Test 1
+                                </div>
+                                <div class="panel-body"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    Test 1
+                                </div>
+                                <div class="panel-body"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    Test 1
+                                </div>
+                                <div class="panel-body"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="panel panel-info">
+                                <div class="panel-heading">
+                                    Test 1
+                                </div>
+                                <div class="panel-body"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<br/>
